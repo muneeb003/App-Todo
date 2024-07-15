c751bf9cf286770aa5e862d047f3559400c73ce2
muneeb003 <mmuneebkhan2003@gmail.com>
2024-07-15 12:37:35 +0500

    sd

--- src/App.js
+++ src/App.js
@@ -1,54 +1,64 @@
-import React ,{useState,useEffect} from 'react';
-import './App.css';
-import Form from './components/Form'
-import TodoList from './components/TodoList'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 function App() {
-  const [inputData,setInputData] = useState("");
-  const [todos,setTodos] = useState([]);
-  const [status,setStatus] = useState('all');
-  const [filteredTodos,setFilteredTodos] = useState([]);
+  const [inputData, setInputData] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
 
   const filterHandler = () => {
-    switch(status){
+    switch (status) {
       case "completed":
         setFilteredTodos(todos.filter((todo) => todo.complete === true));
         break;
-        case "uncompleted":
-          setFilteredTodos(todos.filter((todo) => todo.complete === false));
-          break;
-        default:
-          setFilteredTodos(todos);
-          break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.complete === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
     }
-  }
-  useEffect(() => {getTodos();},[])
-  useEffect(() => {filterHandler();saveLocalTodos();},[todos,status]);
+  };
+  useEffect(() => {
+    getTodos();
+  }, []);
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
 
   const saveLocalTodos = () => {
     if (todos.length > 0) {
-      localStorage.setItem('todos', JSON.stringify(todos)); }
-  }
-  const getTodos = () => {
-    if (localStorage.getItem("todos")===null){
-      localStorage.setItem("todos",JSON.stringify([]));
+      localStorage.setItem("todos", JSON.stringify(todos));
     }
-    else{
+  };
+  const getTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
       let localData = JSON.parse(localStorage.getItem("todos"));
       setTodos(localData);
     }
-  }
+  };
   return (
     <div className="App">
       <header>
         <h1>My TODO List</h1>
       </header>
-      <Form setInputData={setInputData}
-       todos={todos} 
-       setTodos={setTodos} 
-       inputData={inputData} 
-       setStatus={setStatus}
-       />
-      <TodoList todos={todos} setTodos={setTodos} filteredTodos={filteredTodos}/>
+      <Form
+        setInputData={setInputData}
+        todos={todos}
+        setTodos={setTodos}
+        inputData={inputData}
+        setStatus={setStatus}
+      />
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
+        filteredTodos={filteredTodos}
+      />
     </div>
   );
 }
